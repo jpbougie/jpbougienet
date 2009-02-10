d51f05a2e9c1c12c533e83fdcc9987386b0570b1
Jean-Philippe Bougie <jp.bougie@gmail.com>
2009-02-10 22:40:55 +0100

    added the cp server to the apps

--- settings.py
+++ settings.py
@@ -72,6 +72,7 @@ INSTALLED_APPS = (
     'django.contrib.sites',
     'django.contrib.admin',
     'django.contrib.comments',
+    'django_cpserver'
     'tagging',
     'template_utils',
     'blog',
